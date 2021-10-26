c56b8030b385d95c35d371e3b68cecf536239ab1
AdrienDac <adrien.dac31@outlook.fr>
2021-10-26 14:50:39 +0200

    Modification pendant le cours.

--- app/src/main/java/com/example/cours/MainActivity.java
+++ app/src/main/java/com/example/cours/MainActivity.java
@@ -6,8 +6,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private Button  button_1;
@@ -16,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.StartPage);
 
         button_1 = (Button) findViewById(R.id.button_1);
         button_1.setOnClickListener(new View.OnClickListener() {
