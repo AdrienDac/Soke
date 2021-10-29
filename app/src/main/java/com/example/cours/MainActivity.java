238a7cac73179724ee65eb446a503e56b85fd16e
Sulyya <clementdel2@hotmail.fr>
2021-10-29 12:57:57 +0200

    Nouvelle Home page

--- app/src/main/java/com/example/cours/MainActivity.java
+++ app/src/main/java/com/example/cours/MainActivity.java
@@ -6,9 +6,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
-    private Button  button_1;
+    private ImageButton  button_1;
 
 
     @Override
@@ -16,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.homepage);
 
-        button_1 = (Button) findViewById(R.id.button);
+        button_1 = (ImageButton) findViewById(R.id.txt_to_continue);
         button_1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
