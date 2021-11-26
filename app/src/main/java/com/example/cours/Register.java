50028e04fe9cc5f2406c05eba7aa5c04aebf7d98
Adrien <adrien.dac31@outlook.fr>
2021-11-26 16:53:04 +0100

    Ajout login + Register

--- app/src/main/java/com/example/cours/Register.java
+++ app/src/main/java/com/example/cours/Register.java
@@ -39,7 +39,7 @@ public class Register extends AppCompatActivity implements View.OnClickListener 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
-            case
+            //case
         }
     }
 }
