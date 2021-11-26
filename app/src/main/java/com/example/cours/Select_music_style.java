96d3642cb8443a5ad3b47d7c738d5c8c6a5a45c1
clement <clement.delmotte@edu.esiee.fr>
2021-11-26 09:47:59 +0100

    Bouton reggae/funk opérationnels

--- app/src/main/java/com/example/cours/Select_music_style.java
+++ app/src/main/java/com/example/cours/Select_music_style.java
@@ -2,13 +2,47 @@ package com.example.cours;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class Select_music_style extends AppCompatActivity {
-
+    private ImageButton reggae;
+    private ImageButton funk;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_music_style);
+
+        reggae = (ImageButton) findViewById(R.id.ImageReggae);
+        reggae.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                open_activity_reggae();
+            }
+        });
+
+        funk = (ImageButton) findViewById(R.id.ImageFunk);
+        funk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                open_activity_funk();
+            }
+        });
+
+
+    }
+
+
+
+    public void open_activity_reggae(){
+        Intent intent = new Intent(this, infos1.class);
+        startActivity(intent);
+    }
+
+    public void open_activity_funk(){
+        Intent intent = new Intent(this,MainActivity2.class);
+        startActivity(intent);
     }
 }
